d8a8525e144515e934f181644de92d5595637177
Marcos Mansur <marcos.mansur@passeidireto.com>
2022-08-01 00:06:30 -0300

    refact: add path constants to process.py

--- src/const.py
+++ src/const.py
@@ -15,6 +15,10 @@ M_PRE_DROPINC = '[DEBUG] PREPROCESS - DROP INCOMPLETE WEEKS (5/6): DONE!'
 M_PRE_SPLIT2 = '[DEBUG] PREPROCESS - SPLIT DATA INTO 2 FOLDS (6/6): DONE!'
 M_PRE_SPLIT3 = '[DEBUG] PREPROCESS - SPLIT DATA INTO 3 FOLDS (6/6): DONE!'
 
+M_PRO_TRANS = '[DEBUG] PROCESS: TRASFORMING DATASETS (1/3): DONE!'
+M_PRO_SAVE_DATA = '[DEBUG] PROCESS: SAVING DATASETS (2/3): DONE!'
+M_PRO_SAVE_WEEK = '[DEBUG] PROCESS: SAVING WEEK INITIAL DAYS (3/3): DONE!'
+
 
 BATCH_SIZE_PRO = 32
 WINDOW_SIZE_PRO = 7
@@ -23,9 +27,19 @@ TARGET_PERIOD_PRO = 1
 HOW_WINDOW_GEN_PRO = 'autorregressivo'
 
 
-TREATED_DATA_PATH = 'data\treated'
-PROCESSED_DATA_PATH = 'data\processed'
-
+TREATED_DATA_PATH = 'data/treated'
 TRAIN_TREATED_DATA_PATH = 'data/treated/train_preprocessed.csv'
 VAL_TREATED_DATA_PATH = 'data/treated/val_preprocessed.csv'
 TEST_TREATED_DATA_PATH = 'data/treated/test_preprocessed.csv'
+
+
+PROCESSED_DATA_PATH = 'data/processed'
+TRAIN_PROCESSED_DATA_PATH = 'data/processed/train_processed'
+TRAIN_PRED_PROCESSED_DATA_PATH = 'data/processed/train_pred_processed'
+VAL_PROCESSED_DATA_PATH = 'data/processed/val_processed'
+TEST_PROCESSED_DATA_PATH = 'data/processed/test_processed'
+
+TRAIN_PROCESSED_DATA_WEEK_PATH = 'data/processed/train_data_week.csv'
+TRAIN_PRED_PROCESSED_DATA_WEEK_PATH = 'data/processed/train_pred_data_week.csv'
+VAL_PROCESSED_DATA_WEEK_PATH = 'data/processed/val_data_week.csv'
+TEST_PROCESSED_DATA_WEEK_PATH = 'data/processed/test_data_week.csv'
